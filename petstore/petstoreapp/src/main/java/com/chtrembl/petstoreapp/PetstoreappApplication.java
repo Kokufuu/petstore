264a9cbb3d145aebe976c510466fbafe84f2454a
Adam_Brett <adam_brett@epam.com>
2023-11-21 17:10:29 +0100

    added azure app insight

--- petstore/petstoreapp/src/main/java/com/chtrembl/petstoreapp/PetstoreappApplication.java
+++ petstore/petstoreapp/src/main/java/com/chtrembl/petstoreapp/PetstoreappApplication.java
@@ -1,5 +1,6 @@
 package com.chtrembl.petstoreapp;
 
+import com.microsoft.applicationinsights.attach.ApplicationInsights;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -10,9 +11,11 @@ import java.net.UnknownHostException;
 
 @SpringBootApplication
 public class PetstoreappApplication {
+
 	private static Logger logger = LoggerFactory.getLogger(PetstoreappApplication.class);
 
 	public static void main(String[] args) {
+		ApplicationInsights.attach();
 		SpringApplication.run(PetstoreappApplication.class, args);
 		logger.info("PetStoreApp started up... " + System.getProperty("catalina.base"));
 		try {
